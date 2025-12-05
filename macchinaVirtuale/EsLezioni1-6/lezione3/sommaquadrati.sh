34420114cb5b656e9e185d59fbdb79764dd30b53
Nikita Piraino <nikita.piraino@studio.unibo.it>
2025-12-05 11:45:52 +0100

    sommaquadrati now works

--- macchinaVirtuale/EsLezioni1-6/lezione3/sommaquadrati.sh
+++ macchinaVirtuale/EsLezioni1-6/lezione3/sommaquadrati.sh
@@ -1,7 +1,9 @@
-sommma=0
+somma=0
 sommaQ=0
-for i in $@; do
-	((somma=somma+$i))
-	((sommaQ=sommaQ+$(i*i)))
+contatore=0
+for i in "$@"; do
+	((contatore=contatore+1))
+	((somma=somma+contatore))
+	((sommaQ=sommaQ+i*i))
 done
 echo $((sommaQ-somma))
