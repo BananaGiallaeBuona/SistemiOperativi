4357a3e93d6fb8244bfc6357e0aa1781def9f761
Nikita Piraino <nikita.piraino@studio.unibo.it>
2025-12-10 15:41:53 +0100

    leggere.sh is added, it was usefule to understand that the file descriptor isn't only an explanation butis something tha we're used to use

---
+++ macchinaVirtuale/EsLezioni1-6/l4/leggere.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#prendiamo
+#verifichiamo che non sia vuota
+#definiamo il terzo 
+#lo stampiamo
+exec {FileDescriptor}< /usr/include/stdio.h #here we have the file descriptor of the file on the right
+if (($?!=0)); then
+	while read -u ${FileDescriptor} A B C D; do
+		echo "$C"
+	done
+	exec {FileDescriptor}>&-
+fi
