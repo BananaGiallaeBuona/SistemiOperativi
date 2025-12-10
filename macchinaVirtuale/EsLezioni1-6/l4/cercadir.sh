e8a99533427b9cecd75b404f8f799c72835ba9c1
Nikita Piraino <nikita.piraino@studio.unibo.it>
2025-12-10 10:12:36 +0100

    my correctd version of L4.1

--- macchinaVirtuale/EsLezioni1-6/l4/cercadir.sh
+++ macchinaVirtuale/EsLezioni1-6/l4/cercadir.sh
@@ -1,5 +1,7 @@
-find /usr/include -maxdepth 1 -exec 
-	if [[-d && -r && ((-gt ${/usr/include/stdio.h}))]]
-		echo $PATH
-\;
+#!/bin/bash
+for file in ` find /usr/include/* -maxdepth 1`; do 
+	if [[ -d ${file} && -r ${file} && ${file} -nt /usr/include/stdio.h ]]; then
+		echo  /usr/include/${file}
+	fi
+done
 
