10e62a840d5a022144a9d18ba580ad6d98861036
Nikita Piraino <nikita.piraino@studio.unibo.it>
2025-12-10 16:52:42 +0100

    this doesn't print ciao, but print it linear

--- macchinaVirtuale/EsLezioni1-6/l4/leggitutto.sh
+++ macchinaVirtuale/EsLezioni1-6/l4/leggitutto.sh
@@ -2,10 +2,9 @@
 exec {FD}<miofileNoNL.txt
 contatore=0
 if (($? == 0)); then
-	while read -u ${FD}  -n 1 -r A; do
-		((contatore++))
+	while read -u ${FD} -r A; do
+		echo ${A}
 	done;
 	exec {FD}>&-
-	echo  ${contatore}
 else echo "nada"
 fi
