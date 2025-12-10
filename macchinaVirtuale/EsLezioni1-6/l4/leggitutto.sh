2b6f498a94b8854c84374a61a4ccec5a2b91ffdd
Nikita Piraino <nikita.piraino@studio.unibo.it>
2025-12-10 16:45:50 +0100

    adapted, i war realy near to the right versiona lso this time, but i didn't knew hot to use -r

--- macchinaVirtuale/EsLezioni1-6/l4/leggitutto.sh
+++ macchinaVirtuale/EsLezioni1-6/l4/leggitutto.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 exec {FD}<miofileNoNL.txt
+contatore=0
 if (($? == 0)); then
-	while read -u ${FD} A; do
-		echo ${A}
+	while read -u ${FD}  -n 1 -r A; do
+		((contatore++))
 	done;
 	exec {FD}>&-
-else echo "nad"
+	echo  ${contatore}
+else echo "nada"
 fi
