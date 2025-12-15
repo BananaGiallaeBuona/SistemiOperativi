88082623c939bc23b936ee756f580cfeab4484f0
Nikita Piraino <nikita.piraino@studio.unibo.it>
2025-12-15 15:29:50 +0100

    now works

--- macchinaVirtuale/EsLezioni1-6/l5/random.sh
+++ macchinaVirtuale/EsLezioni1-6/l5/random.sh
@@ -1,7 +1,11 @@
 var=$RANDOM
 contatore=0
-while  ((var%10 == 2)); do
-	((contatore=contatore+1))
-	var=$RANDOM
+while  true; do
+	if ((var%10 == 2)); then 
+		break
+	else
+		((contatore=contatore+1))
+		var=$RANDOM
+	fi;
 done
 echo  ${contatore}
