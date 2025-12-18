1cd47fecb7c60c52a358d34b7a0bc2835a215b5e
Nikita Piraino <nikita.piraino@studio.unibo.it>
2025-12-18 15:56:16 +0100

    i'v edon 45.46-47

--- macchinaVirtuale/EsLezioni1-6/l6/accorpamento.sh
+++ macchinaVirtuale/EsLezioni1-6/l6/accorpamento.sh
@@ -1,5 +1,24 @@
+contatore=44
 #45
 sed -n '3,5p' /usr/include/stdio.h
+((contatore++))
 if [ $? -eq 0 ]; then 
-	echo '45 was a good job!'
+	echo "${contatore} was a good job!"
+fi
+
+#46
+tail -4 /usr/include/stdio.h | cut -c 1-3
+((contatore++))
+if [ $? -eq 0 ]; then 
+        echo "${contatore} was a good job!"
+fi
+
+#47
+read primo \n
+read secondo
+echo ${secondo} 
+echo " ${primo}"
+((contatore++))
+if [ $? -eq 0 ]; then 
+        echo "${contatore} was a good job!"
 fi
