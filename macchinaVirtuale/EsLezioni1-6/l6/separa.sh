b4ab2a821b474d86863c403a2b5697fd00e3a1b1
Nikita Piraino <nikita.piraino@studio.unibo.it>
2025-12-19 11:53:57 +0100

    now ex57 does what is requested, but then remains in the loop and sshuts 00000000000

--- macchinaVirtuale/EsLezioni1-6/l6/separa.sh
+++ macchinaVirtuale/EsLezioni1-6/l6/separa.sh
@@ -1,3 +1,10 @@
+i=1
+while pt=`echo "${PATH}" | cut  --delimiter=':' -f $i`; do 
+	echo "${pt} ${#pat}"
+	((i++))
+done
+
+echo 2
 for pat in `echo "${PATH}" | cut  --delimiter=':' -f 1-`; do 
 	echo "${pat} ${#pat}"
 done
