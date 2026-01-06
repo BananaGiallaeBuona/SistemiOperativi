80c6e04c06f8dfd8c788badaadfe0858e1d703c4
Nikita Piraino <nikita.piraino@studio.unibo.it>
2026-01-06 10:45:53 +0100

    the main error was that in the condition [[]] you must put the space before and after the ==

--- macchinaVirtuale/esame-22-12/nikita.piraino/es285/sommase.sh
+++ macchinaVirtuale/esame-22-12/nikita.piraino/es285/sommase.sh
@@ -1,13 +1,9 @@
-!/bin/bash
+#!/bin/bash
 
 # NIKITA, PEZZO DI DELINQUENTE, HAI TOLTO il #
 # NELLA PRIMA RIGA CGE DEFINISCE L'INTERPRETE DEI COMANDI
 # E QUESTO PROVOCA ERRORE A RUN-TIME
 
-#input: niente
-#richie:selezionare per nome
-#selezionare numero terza riga e sommarli+
-#OUtput: la somma
 contatore=0
 #il probelma è che la condizione non scarta come dovrebbe
 for file in `find dir1 -mindepth 1`; do #con questo ciclo stampo il contenuto di fiu>
@@ -15,7 +11,7 @@ for file in `find dir1 -mindepth 1`; do #con questo ciclo stampo il contenuto di
 	# ERRORE SINTATTICO, prova a mettere degli spazi attorno agli ==
 	# e a mettere  dir1/*a* invece di *a*
        
-        if [[ "${file}"=="*a*" || "${file}"=="*f*" ]]; then
+        if [[ "${file}" == *a* || "${file}" == *f* ]]; then
                 numero=`cat "${file}" | sed -n "3p"` 
                 ((contatore+=numero))
         fi
